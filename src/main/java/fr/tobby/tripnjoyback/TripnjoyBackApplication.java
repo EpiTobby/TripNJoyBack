d0a61b78f97fc3fc680ce602c9706142c52f1ca6
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-02-21 14:18:47 +0100

    init

---
+++ src/main/java/fr/tobby/tripnjoyback/TripnjoyBackApplication.java
@@ -0,0 +1,13 @@
+package fr.tobby.tripnjoyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripnjoyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripnjoyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/tobby/tripnjoyback/TripnjoyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tobby.tripnjoyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripnjoyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
