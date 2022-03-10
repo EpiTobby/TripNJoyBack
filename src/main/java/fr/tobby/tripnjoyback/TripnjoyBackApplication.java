8a75d1170e614e9c29c281f8d8ee2e51d35ab253
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-03-10 09:23:01 +0100

    remove test in main class

--- src/main/java/fr/tobby/tripnjoyback/TripnjoyBackApplication.java
+++ src/main/java/fr/tobby/tripnjoyback/TripnjoyBackApplication.java
@@ -1,11 +1,6 @@
 package fr.tobby.tripnjoyback;
 
-import fr.tobby.tripnjoyback.entity.GenderEntity;
-import fr.tobby.tripnjoyback.repository.CityRepository;
-import fr.tobby.tripnjoyback.repository.GenderRepository;
-import fr.tobby.tripnjoyback.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,20 +14,6 @@ public class TripnjoyBackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripnjoyBackApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner test(UserRepository userRepository, GenderRepository genderRepository, CityRepository cityRepository) {
-		return args -> {
-
-			GenderEntity gender = genderRepository.findByValue("male").get();
-			System.out.println(gender.getId());
-			// var city = cityRepository.save(new CityEntity("Paris"));
-
-			// userRepository.save(new UserEntity("coucou", "Robert", "passwd", "truc", Instant.now(), gender, "", city, null));
-
-			userRepository.findAll().forEach(user -> System.out.println(user.getId()));
-		};
 	}
 
 	@Bean
