9b60fb21fd93cfbb23f460a8577215ffeb1450dc
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-03-12 16:21:54 +0100

    security: persist session

--- src/main/java/fr/tobby/tripnjoyback/controller/LoginController.java
+++ src/main/java/fr/tobby/tripnjoyback/controller/LoginController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -36,7 +37,7 @@ public class LoginController {
         Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         if (authenticate.isAuthenticated())
         {
-            // Save session
+            SecurityContextHolder.getContext().setAuthentication(authenticate);
         }
         return authenticate;
     }
