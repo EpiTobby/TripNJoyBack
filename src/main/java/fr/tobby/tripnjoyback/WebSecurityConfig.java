7f990d84552e055c17c21a73cae6db3e20c4ae47
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-03-17 09:25:51 +0100

    security (fix): allow all on /auth/**

--- src/main/java/fr/tobby/tripnjoyback/WebSecurityConfig.java
+++ src/main/java/fr/tobby/tripnjoyback/WebSecurityConfig.java
@@ -32,12 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception
     {
         http.authorizeHttpRequests()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/login/create").permitAll()
-                .antMatchers("/users/create").permitAll()
-                .antMatchers("/users/forgotpassword").permitAll()
-                .antMatchers("/users/validatecodepassword").permitAll()
-                .antMatchers("/users/{id}/updatepassword").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
             .and()
             // Do not persist session. Auth is done via jwt and checked at each request
