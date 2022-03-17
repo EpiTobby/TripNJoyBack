2f56ce55dfe0f925a94d76be467705da28f0c9c9
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-03-17 10:10:48 +0100

    swagger: document login and register

--- src/main/java/fr/tobby/tripnjoyback/controller/AuthController.java
+++ src/main/java/fr/tobby/tripnjoyback/controller/AuthController.java
@@ -2,7 +2,7 @@ package fr.tobby.tripnjoyback.controller;
 
 import fr.tobby.tripnjoyback.exception.UserCreationException;
 import fr.tobby.tripnjoyback.model.ConfirmationCodeModel;
-import fr.tobby.tripnjoyback.model.UserCreationModel;
+import fr.tobby.tripnjoyback.model.UserCreationRequest;
 import fr.tobby.tripnjoyback.model.UserModel;
 import fr.tobby.tripnjoyback.model.request.ForgotPasswordRequest;
 import fr.tobby.tripnjoyback.model.request.UpdatePasswordRequest;
@@ -11,6 +11,8 @@ import fr.tobby.tripnjoyback.model.request.auth.LoginRequest;
 import fr.tobby.tripnjoyback.model.response.UserIdResponse;
 import fr.tobby.tripnjoyback.model.response.auth.LoginResponse;
 import fr.tobby.tripnjoyback.service.AuthService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
@@ -27,12 +29,18 @@ public class AuthController {
     }
 
     @PostMapping("register")
-    public UserModel create(@RequestBody UserCreationModel model)
+    @ApiOperation(value = "Create a new account. Will send a confirmation mail to the given address")
+    @ApiResponse(responseCode = "200", description = "User is created")
+    @ApiResponse(responseCode = "422", description = "If the email is already in use by another user")
+    public UserModel create(@RequestBody UserCreationRequest model)
     {
         return authService.createUser(model);
     }
 
     @PostMapping("login")
+    @ApiOperation("Log a user, to allow authenticated endpoints")
+    @ApiResponse(responseCode = "401", description = "Authentication failed. Wrong username or password")
+    @ApiResponse(responseCode = "200", description = "Authentication Succeeded. Use the given jwt in following requests")
     public LoginResponse login(@RequestBody LoginRequest loginRequest)
     {
         String token = authService.login(loginRequest.getUsername(), loginRequest.getPassword());
