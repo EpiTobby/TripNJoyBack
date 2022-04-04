912ac09c1d7db6e07208f7415f3d65e2357b48c0
GabrielBouhnik <gabriel.bouhnik@epita.fr>
2022-04-04 17:36:31 +0200

    add group entity and state entity

---
+++ src/main/java/fr/tobby/tripnjoyback/service/GroupService.java
@@ -0,0 +1,11 @@
+package fr.tobby.tripnjoyback.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupService {
+    private static final Logger logger = LoggerFactory.getLogger(GroupService.class);
+
+}
