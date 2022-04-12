a45fcbde98904e9e3c18c892e21b00f1864c22ba
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-04-12 21:15:07 +0200

    group repository: find available groups

--- src/main/java/fr/tobby/tripnjoyback/repository/GroupRepository.java
+++ src/main/java/fr/tobby/tripnjoyback/repository/GroupRepository.java
@@ -1,10 +1,18 @@
 package fr.tobby.tripnjoyback.repository;
 
 import fr.tobby.tripnjoyback.entity.GroupEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface GroupRepository extends CrudRepository<GroupEntity, Long> {
-    Optional<GroupEntity> findById(long  groupId);
+    Optional<GroupEntity> findById(long groupId);
+
+    @Query("SELECT g FROM GroupEntity g "
+            + "JOIN StateEntity state ON g.stateEntity = state JOIN GroupMemberEntity member ON member.group = g"
+            + " WHERE state.value = 'open'"
+            + " GROUP BY g HAVING count(g) < g.maxSize")
+    Collection<GroupEntity> findAvailableGroups();
 }
