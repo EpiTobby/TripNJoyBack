f2ab248f7f0a8db85d133db1199ac7892ffda86a
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-04-13 12:12:43 +0200

    matchmaking: match groups

--- src/main/java/fr/tobby/tripnjoyback/service/MatchMaker.java
+++ src/main/java/fr/tobby/tripnjoyback/service/MatchMaker.java
@@ -1,20 +1,24 @@
 package fr.tobby.tripnjoyback.service;
 
 import fr.tobby.tripnjoyback.entity.AnswersEntity;
+import fr.tobby.tripnjoyback.entity.GroupEntity;
 import fr.tobby.tripnjoyback.entity.ProfileEntity;
 import fr.tobby.tripnjoyback.entity.UserEntity;
 import fr.tobby.tripnjoyback.model.MatchMakingUserModel;
 import fr.tobby.tripnjoyback.model.ProfileModel;
 import fr.tobby.tripnjoyback.model.request.anwsers.RangeAnswerModel;
 import fr.tobby.tripnjoyback.repository.AnswersRepository;
+import fr.tobby.tripnjoyback.repository.GroupRepository;
 import fr.tobby.tripnjoyback.repository.ProfileRepository;
 import fr.tobby.tripnjoyback.repository.UserRepository;
 import fr.tobby.tripnjoyback.utils.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,21 +29,48 @@ public class MatchMaker {
     private final AnswersRepository answersRepository;
     private final MatchMakerScoreComputer scoreComputer;
     private final GroupService groupService;
+    private final GroupRepository groupRepository;
 
     public MatchMaker(final ProfileRepository profileRepository, final UserRepository userRepository,
                       final AnswersRepository answersRepository, final MatchMakerScoreComputer scoreComputer,
-                      final GroupService groupService)
+                      final GroupService groupService, final GroupRepository groupRepository)
     {
         this.profileRepository = profileRepository;
         this.userRepository = userRepository;
         this.answersRepository = answersRepository;
         this.scoreComputer = scoreComputer;
         this.groupService = groupService;
+        this.groupRepository = groupRepository;
     }
 
+    @Transactional
     public void match(@NotNull final MatchMakingUserModel user)
     {
         // TODO: Find groups
+        Collection<GroupEntity> groups = groupRepository.findAvailableGroups();
+        Optional<GroupEntity> matchedGroup = groups.stream()
+                                                   .map(group -> {
+                                                       long profileId = group.getProfile().getId();
+                                                       AnswersEntity groupAnswers = answersRepository.findByProfileId(profileId);
+
+                                                       ProfileModel profileModel = ProfileModel.of(group.getProfile(), groupAnswers);
+                                                       return new Pair<>(group, profileModel);
+                                                   })
+                                                   .filter(pair -> scoreComputer.isUserCompatible(pair.right(), user))
+                                                   .map(pair -> {
+                                                       float score = scoreComputer.computeMatchingScore(user.getProfile(), pair.right());
+                                                       return new Pair<>(pair.left(), score);
+                                                   })
+                                                   .filter(pair -> pair.right() > 0) // TODO: replace '0' by minimal matching score
+                                                   .max(Comparator.comparingDouble(Pair::right))
+                                                   .map(Pair::left);
+
+        if (matchedGroup.isPresent())
+        {
+            // TODO: Join group
+            return;
+        }
+
 
         Collection<MatchMakingUserModel> others = userRepository.findAllByWaitingForGroupIsTrue()
                                                                 .stream()
