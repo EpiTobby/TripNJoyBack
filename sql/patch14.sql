96a7b9112a31d6ccfe3fe2be6f26fb812492a6aa
Yanis Chaabane <yanis.chaabane@epita.fr>
2022-04-21 11:22:24 +0200

    fix: remove drop constraint

--- sql/patch14.sql
+++ sql/patch14.sql
@@ -2,8 +2,5 @@ alter table users_groups
     add id serial;
 
 alter table users_groups
-    drop constraint users_groups_pk;
-
-alter table users_groups
     add constraint users_groups_pk
         primary key (id);
