60b61044e73bf1d8d619e2b5cadf9b99045d1d15
GabrielBouhnik <gabriel.bouhnik@epita.fr>
2022-05-23 14:49:01 +0200

    prevent deletion of last channel in group

---
+++ src/main/java/fr/tobby/tripnjoyback/exception/DeleteChannelException.java
@@ -0,0 +1,10 @@
+package fr.tobby.tripnjoyback.exception;
+
+public class DeleteChannelException extends RuntimeException{
+    public DeleteChannelException() {
+    }
+
+    public DeleteChannelException(String message) {
+        super(message);
+    }
+}
