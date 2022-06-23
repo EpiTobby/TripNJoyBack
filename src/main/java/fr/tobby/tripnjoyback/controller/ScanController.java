22b762428eb755a4044c297cac34cec511f20dba
Yanis Chaabane <yanis.chaabane@epita.fr>
2022-06-23 17:33:37 +0200

    fix(scan): using correct decorators for controller

--- src/main/java/fr/tobby/tripnjoyback/controller/ScanController.java
+++ src/main/java/fr/tobby/tripnjoyback/controller/ScanController.java
@@ -6,8 +6,11 @@ import fr.tobby.tripnjoyback.service.ScanService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("scan")
+@RestController
+@RequestMapping(path = "/scan")
 public class ScanController {
 
     private final ScanService scanService;
