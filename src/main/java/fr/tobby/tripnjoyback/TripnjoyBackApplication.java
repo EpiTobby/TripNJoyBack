d9aa7ee5a909959697c7f49b700e33269a1e0a6b
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-07-09 11:38:55 +0200

    services: fix SonarCloud issues

--- src/main/java/fr/tobby/tripnjoyback/TripnjoyBackApplication.java
+++ src/main/java/fr/tobby/tripnjoyback/TripnjoyBackApplication.java
@@ -1,5 +1,6 @@
 package fr.tobby.tripnjoyback;
 
+import fr.tobby.tripnjoyback.service.ScanService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,10 +25,14 @@ public class TripnjoyBackApplication {
 	}
 
 	@Bean
-	public CommandLineRunner startup(@Value("${spring.profiles.active}") String profiles)
+	public CommandLineRunner startup(@Value("${spring.profiles.active}") String profiles, ScanService scanService)
 	{
 		return args -> {
 			logger.info("Application started with profiles {}", profiles);
+//			ScanResponse scan = scanService.scan("https://images-ext-1.discordapp.net/external/J3_EjXEM31n8QSyedYy5V3MqJNiffbfs7EFmopdu85s/https/ocr.space/Content/Images/receipt-ocr-original.jpg?width=459&height=1138");
+//
+//			scan.getItems().forEach((name, price) -> logger.info("{}: {}", name, price));
+//			logger.info("Total: {}", scan.getTotal());
 		};
 	}
 }
