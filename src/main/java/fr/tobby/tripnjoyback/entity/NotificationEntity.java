4ad7c145292e06e673b359946be2af0a79ae85d6
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-09-20 12:08:39 +0200

    notifications (fix): specify table name

--- src/main/java/fr/tobby/tripnjoyback/entity/NotificationEntity.java
+++ src/main/java/fr/tobby/tripnjoyback/entity/NotificationEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "notifications")
 public class NotificationEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
