6195c1b2399d5ebac321d2717ab7f45b86d599b2
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-10-21 16:22:35 +0200

    services: add eureka discovery service

---
+++ modules/eureka/src/test/java/fr/tripnjoy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.tripnjoy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
