ac36d2b6bdc4a9c34196a5c4fc0773e157606584
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2022-10-23 13:56:49 +0200

    services: add config server

---
+++ modules/configserver/src/test/java/fr/tripnjoy/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,14 @@
+package fr.tripnjoy.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
