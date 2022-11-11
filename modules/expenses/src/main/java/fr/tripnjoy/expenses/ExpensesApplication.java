61c5e638840b007331559d73149ab33a38bccac3
Gabriel Rayzal <gabriel.rayzal@epita.fr>
2022-11-11 16:40:39 +0100

    expenses service (fix): auto scan fr.tripnjoy.users for feign clients

--- modules/expenses/src/main/java/fr/tripnjoy/expenses/ExpensesApplication.java
+++ modules/expenses/src/main/java/fr/tripnjoy/expenses/ExpensesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients(basePackages = "fr.tripnjoy.groups")
+@EnableFeignClients(basePackages = {"fr.tripnjoy.groups", "fr.tripnjoy.users"})
 public class ExpensesApplication {
 
     public static void main(String[] args)
