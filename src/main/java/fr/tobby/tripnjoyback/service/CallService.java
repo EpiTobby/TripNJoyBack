f82bef2fdadedea4474439361c08520b664deb76
Tony Heng <tony.heng@epita.fr>
2022-11-14 18:24:41 +0100

    feat: calls: add call Controller and Service, add Agora dependencies

--- src/main/java/fr/tobby/tripnjoyback/service/CallService.java
+++ src/main/java/fr/tobby/tripnjoyback/service/CallService.java
@@ -2,21 +2,23 @@ package fr.tobby.tripnjoyback.service;
 
 import io.agora.media.RtcTokenBuilder;
 import io.agora.media.RtcTokenBuilder.Role;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CallService {
-    static String appId = "6cbc8e0499d64486aa09738b9c326c81";
-    static String appCertificate = "472bedcb37df47a5b98ec1feccb8d01d";
-    static int expirationTimeInSeconds = 3600; // The time after which the token expires
+    String appId;
+    String appCertificate;
+    static int expirationTimeInSeconds = 3600;
+
+    public CallService(@Value("${agora.app.id}") String appId, @Value("${agora.app.certificate}") String appCertificate) {
+        this.appId = appId;
+        this.appCertificate = appCertificate;
+    }
 
     public String generateToken(String channelName, int uid) {
         RtcTokenBuilder tokenBuilder = new RtcTokenBuilder();
         int timestamp = (int)(System.currentTimeMillis() / 1000 + expirationTimeInSeconds);
-        String result = tokenBuilder.buildTokenWithUid(appId, appCertificate,
-                channelName, uid, Role.Role_Attendee, timestamp);
-        System.out.println("UID token:");
-        System.out.println(result);
-        return result;
+        return tokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, Role.Role_Attendee, timestamp);
     }
 }
